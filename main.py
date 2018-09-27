83d75f6bee360aa1e6ff2ee1d533c8438daafbd2
BarnaSir <sudarshanregmi9@gmail.com>
2018-09-27 15:46:42 +0545

    working model, dirty implementation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sys import maxsize
 from math import hypot
 from itertools import cycle
+from time import sleep
 
 from tkinter import *
 
@@ -48,17 +49,17 @@ def draw_grid():
 
 
 def update(event):
-    global FilledUp
+    global FilledUp, current_player
     if len(player_1.owned_position) == 3 and len(player_2.owned_position) == 3:
         FilledUp = 1
     if FilledUp != None:
         return
     toggle_turn()
     print("entered click event as ", current_player.name)
-    global current_player
     x, y = event.x, event.y
     (a, b) = nearest_node(x, y)
     if a is None or not is_empty(a, b):
+        toggle_turn()
         return 0
     if all_filled():
         print("all are filled")
@@ -83,6 +84,11 @@ def update(event):
         # current_player = players.__next__()
         # toggle_turn()
         pass
+    if check_game():
+        print("Game won by ", current_player.name)
+        status['text'] = "the window will be closed soon"
+        canvas.unbind("<Button-1>")
+        canvas.unbind("<Double-Button-1>")
 
 def nearest_node(x, y):
     temp = maxsize
@@ -97,10 +103,12 @@ def nearest_node(x, y):
 
 
 def double_click(event):
+    global xPress, yPress, current_player, FilledUp
+    if FilledUp != 1:
+        return 0
     print("entered double click fxn")
     # current_player = toggle_turn()
     print("entered as", current_player.name)
-    global xPress, yPress, current_player
     a, b = nearest_node(event.x, event.y)
     if a is None: return
     if xPress == None and is_empty(a, b):
@@ -132,6 +140,16 @@ def double_click(event):
         current_player.owned_position[oval_obj] = (a, b)
         # toggle_turn()
         xPress = yPress = None
+    print("position owned by "+current_player.name+" are " + str(current_player.owned_position))
+    if check_game():
+        print("Game won by ", current_player.name)
+        status['text'] = "the window will be closed soon"
+        # exit(0)
+        canvas.unbind("<Button-1>")
+        canvas.unbind("<Double-Button-1>")
+
+def game_finished():
+    print("game over")
 
 def toggle_turn():
     global current_player
@@ -151,8 +169,18 @@ def get_oval_obj_key(x, y, current_player):
         if val == (x, y):
             return key
 
-def check():
-    pass
+def check_game():
+    global current_player
+    check_list = tuple(current_player.owned_position.values())
+    if len(check_list) != 3:
+        return 0
+    return (check_list[0][0] == check_list[1][0] == check_list[2][0]) \
+           or (check_list[0][1] == check_list[1][1] == check_list[2][1]) \
+           or ((check_list[0][0] == check_list[0][1]) and (check_list[1][0] == check_list[1][1]) and (check_list[2][0] == check_list[2][1])) \
+           or (((check_list[0][0] == check_list[0][1]) or (check_list[1][0] == check_list[1][1]) or (check_list[2][0] == check_list[2][1])) and addfn(check_list))
+
+def addfn(check_list):
+    return check_list[0] == check_list[1][::-1] or check_list[0] == check_list[2][::-1] or check_list[1] == check_list[2][::-1]
 
 def is_empty(x, y):
     if player_1.owned_position or player_2.owned_position:
@@ -174,11 +202,11 @@ draw_grid()
 print(POINTS)
 player_1 = Player("Sudarshan", "Blue")
 player_2 = Player("Barna", "Yellow")
-# current_player = player_1
 players_list = {player_1: player_2, player_2: player_1}
 players = cycle([player_1, player_2])
 status = Label(root, text="It's "+player_1.name+"'s turn now!" , bd=1, relief=SUNKEN, anchor=W)
 status.pack(side=BOTTOM, fill=X)
+toggle_turn()
 canvas.bind('<Double-Button-1>', double_click)
 # canvas.bind('<ButtonPress-1>', one)
 canvas.bind('<ButtonPress-1>', update)
