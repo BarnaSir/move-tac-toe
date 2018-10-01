62a300795e8467db97766c2c80c0ecb94b69cf21
BarnaSir <sudarshanregmi9@gmail.com>
2018-10-01 17:39:20 +0545

    coordinate of selected oval made equal to current cursor position

--- main.py
+++ main.py
@@ -55,15 +55,15 @@ class Player:
 def draw_grid():
     for i in range(8):
         canvas.create_line(GRID_POINTS[i][0], GRID_POINTS[i][1], GRID_POINTS[i][2],
-                           GRID_POINTS[i][3])
+                           GRID_POINTS[i][3], width=3)
 
 
 def result():
-    print("Game won by ", current_player.name)
-    status['text'] = "Game Over!!!  " + current_player.name + " wins."
+    print("Game won by ", current_player.color_notation)
+    status['text'] = "Game Over!  " + current_player.color_notation + " wins the game"
     canvas.unbind("<Button-1>")
     canvas.unbind("<Double-Button-1>")
-    messagebox.showinfo("Game Over!!! ", current_player.name + " wins")
+    messagebox.showinfo("Game Over!!! ", current_player.color_notation + " wins the game")
 
 
 def get_nearest_node(x, y):
@@ -95,7 +95,7 @@ def check_moving_condition(a, b, x_prev, y_prev):
 
 
 def move_a_piece(a, b, x_prev, y_prev):
-    oval_obj = canvas.create_oval(a-20, b-20, a+20, b+20, fill=current_player.color_notation)
+    oval_obj = canvas.create_oval(a-30, b-30, a+30, b+30, fill=current_player.color_notation)
     canvas.delete(get_oval_obj_key(x_prev, y_prev, current_player))
     current_player.owned_position.pop(get_oval_obj_key(x_prev, y_prev, current_player), None)
     current_player.owned_position[oval_obj] = (a, b)
@@ -109,7 +109,7 @@ def legal_move(x, y, x_prev, y_prev):
     print(VALID_MOVES[previous_index])
     if current_index in VALID_MOVES[previous_index]:
         return True
-    info['text'] = "INVALID MOVE"
+    info['text'] = "Invalid Move  "
     return False
 
 
@@ -132,9 +132,9 @@ def toggle_turn():
 
 def status_bar():
     if current_player == player_1:
-        status['text'] = player_2.name + "(" + player_2.color_notation + ")"
+        status['text'] =  player_2.color_notation
     else:
-        status['text'] = player_1.name + "(" + player_1.color_notation + ")"
+        status['text'] = player_1.color_notation
 
 
 def exhaust_single_click():
@@ -175,6 +175,10 @@ def all_filled():
         return 1
 
 
+def bring_oval_in_motion(event, index):
+    canvas.coords(index, event.x-30, event.y-30, event.x+30, event.y+30)
+
+
 def single_click_update(event):
     global ALLOW_SINGLE_CLICK, current_player
     if exhaust_single_click():  # prevent single click if it's exhausted
@@ -187,7 +191,7 @@ def single_click_update(event):
     if a is None or not is_empty(a, b):
         toggle_turn()
         return
-    oval_obj = canvas.create_oval(a-20, b-20, a+20, b+20, fill=current_player.color_notation)
+    oval_obj = canvas.create_oval(a-30, b-30, a+30, b+30, fill=current_player.color_notation)
     current_player.owned_position[oval_obj] = (a, b)
     status_bar()
     if check_game():
@@ -201,30 +205,32 @@ def double_click_update(event):
     if (ALLOW_SINGLE_CLICK != 0) or (invalid_double_click(a, b, xprev)) \
             or (a == xprev and b == yprev):
         if a == xprev and b == yprev:
-            info['text'] = "Can't move to same position"
+            info['text'] = "Can't move to same position.  "
         return
     if ONE_TIME_CONSTANT == 1:
         ONE_TIME_CONSTANT -= 1
         toggle_turn()
     if not is_empty(a, b):
         if xprev is None and not own_cell(a, b, current_player):
-            info['text'] = "That's not your piece"
+            info['text'] = "That's not your piece.  "
             return
         elif xprev is not None:
-            info['text'] = "INVALID MOVE"
+            info['text'] = "INVALID MOVE   "
             return
     info['text'] = ""
     if is_obj_movable(a, b, xprev, current_player, ALLOW_SINGLE_CLICK):
         xprev, yprev = a, b
         current_index = POINTS.index((a, b))
         if not valid_move_has_empty_cell(VALID_MOVES[current_index]):
-            info['text'] = "IMMOVABLE"
+            info['text'] = "Immovable  "
             xprev, yprev = None, None
             return 0
-        canvas.itemconfig(get_oval_obj_key(a, b, current_player), outline="black", width=5)
+        canvas.itemconfig(get_oval_obj_key(a, b, current_player), outline="#53f481", width=10)
+        index_of_obj = get_oval_obj_key(a, b, current_player)
+        canvas.bind("<Motion>", lambda event: bring_oval_in_motion(event, index_of_obj))
     elif check_moving_condition(a, b, xprev, yprev):
         move_a_piece(a, b, xprev, yprev)
-
+        canvas.unbind("<Motion>")
         if check_game():
             result()
         else:
@@ -232,17 +238,44 @@ def double_click_update(event):
         toggle_turn()
         xprev = yprev = None
 
+def move(event):
+    pass
+
+def close():
+    root.destroy()
+
+
+def help_game():
+    messagebox.showinfo("Help", "Welcome to the movable tic-tac-toe game. "
+                                "The playing rules and win conditions are same "
+                                "as that of Tic-Tac-Toe game except the limitation of number of pieces. "
+                                "Each player has 3 pieces and after s/he has put all the "
+                                "pieces in the board, s/he should drag the pieces. "
+                                "Single click to put the piece in the board and double click to drag. "
+                                "Double click on the position again to place the dragged piece. "
+                                )
+
 
 draw_grid()
 player_1 = Player("Sudarshan", "Blue")
-player_2 = Player("Barna", "Yellow")
+player_2 = Player("Barna", "Red")
 players = cycle([player_1, player_2])
-info = Label(root, text="", font="Times 13")
+info = Label(root, text="", font="Times 12 bold")
 info.pack(side=RIGHT)
-turn = Label(root, text="Turn: ", font="Roboto 12")
+turn = Label(root, text="  Turn:", font="Times 12 bold", padx=0, pady=5)
 turn.pack(side=LEFT, fill=X)
-status = Label(root, text=player_1.name +'('+player_1.color_notation+')', font="Times 12 bold", padx=5, pady=5, bd=6, relief="groove")
+status = Label(root, text=player_1.color_notation , font="Times 12 bold", padx=0, pady=5, bd=6)
 status.pack(side=LEFT, fill=X)
 canvas.bind('<Double-Button-1>', double_click_update)
 canvas.bind('<ButtonPress-1>', single_click_update)
+canvas.bind('ButtonPress-2', move)
+
+menu_bar = Menu(root)
+root.config(menu=menu_bar)
+
+file_menu =  Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="Exit", command=close)
+menu_bar.add_cascade(label="File", menu=file_menu)
+menu_bar.add_command(label="Help", command=help_game)
+
 root.mainloop()
